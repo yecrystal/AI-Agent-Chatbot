130068b9b1e861c1ea68aec13b6b6d88a7f949ca
yecrystal <crystalllye@gmail.com>
2025-04-20 14:52:22 -0400

    Initial commit

---
+++ ai_agent.py
@@ -0,0 +1,44 @@
+# if you dont use pipenv uncomment the following:
+from dotenv import load_dotenv
+load_dotenv()
+
+#Step1: Setup API Keys for Groq, OpenAI and Tavily
+import os
+
+GROQ_API_KEY=os.environ.get("GROQ_API_KEY")
+TAVILY_API_KEY=os.environ.get("TAVILY_API_KEY")
+OPENAI_API_KEY=os.environ.get("OPENAI_API_KEY")
+
+#Step2: Setup LLM & Tools
+from langchain_groq import ChatGroq
+from langchain_openai import ChatOpenAI
+from langchain_community.tools.tavily_search import TavilySearchResults
+
+openai_llm=ChatOpenAI(model="gpt-4o-mini")
+groq_llm=ChatGroq(model="llama-3.3-70b-versatile")
+
+search_tool=TavilySearchResults(max_results=2)
+
+#Step3: Setup AI Agent with Search tool functionality
+from langgraph.prebuilt import create_react_agent
+from langchain_core.messages.ai import AIMessage
+
+system_prompt="Act as an AI chatbot who is smart and friendly"
+
+def get_response_from_ai_agent(llm_id, query, allow_search, system_prompt, provider):
+    if provider=="Groq":
+        llm=ChatGroq(model=llm_id)
+    elif provider=="OpenAI":
+        llm=ChatOpenAI(model=llm_id)
+
+    tools=[TavilySearchResults(max_results=2)] if allow_search else []
+    agent=create_react_agent(
+        model=llm,
+        tools=tools,
+        state_modifier=system_prompt
+    )
+    state={"messages": query}
+    response=agent.invoke(state)
+    messages=response.get("messages")
+    ai_messages=[message.content for message in messages if isinstance(message, AIMessage)]
+    return ai_messages[-1]
---
+++ backend.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+from typing import List
+
+class RequestState(BaseModel):
+    model_name: str
+    model_provider: str
+    system_prompt: str
+    messages: List[str]
+    allow_search: bool
+
+from fastapi import FastAPI
+from ai_agent import get_response_from_ai_agent
+
+ALLOWED_MODEL_NAMES = ["llama3-70b-8192", "mixtral-8x7b-32768", "llama-3.3-70b-versatile", "gpt-4o-mini"]
+
+app = FastAPI(title="LangGraph AI Agent")
+
+@app.post("/chat")
+def chat_endpoint(request: RequestState):
+    """
+    API Endpoint to interact with the Chatbot using LangGraph and search tools.
+    It dynamically selects the model specified in the request.
+    """
+    if request.model_name not in ALLOWED_MODEL_NAMES:
+        return {"error": "Model not allowed. Please use one of the allowed models."}
+    
+    response=get_response_from_ai_agent
