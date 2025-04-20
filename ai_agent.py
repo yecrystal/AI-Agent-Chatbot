a00c8761ed05e14ab4470bb0774514d20bf6d515
yecrystal <crystalllye@gmail.com>
2025-04-20 18:26:23 -0400

    Added backend.py

--- ai_agent.py
+++ ai_agent.py
@@ -1,4 +1,3 @@
-# if you dont use pipenv uncomment the following:
 from dotenv import load_dotenv
 load_dotenv()
 
--- backend.py
+++ backend.py
@@ -1,5 +1,10 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+#Step1: Setup Pydantic Model (Schema Validation)
 from pydantic import BaseModel
 from typing import List
+
 
 class RequestState(BaseModel):
     model_name: str
@@ -8,20 +13,35 @@ class RequestState(BaseModel):
     messages: List[str]
     allow_search: bool
 
+
+#Step2: Setup AI Agent from FrontEnd Request
 from fastapi import FastAPI
 from ai_agent import get_response_from_ai_agent
 
-ALLOWED_MODEL_NAMES = ["llama3-70b-8192", "mixtral-8x7b-32768", "llama-3.3-70b-versatile", "gpt-4o-mini"]
+ALLOWED_MODEL_NAMES=["llama3-70b-8192", "mixtral-8x7b-32768", "llama-3.3-70b-versatile", "gpt-4o-mini"]
 
-app = FastAPI(title="LangGraph AI Agent")
+app=FastAPI(title="LangGraph AI Agent")
 
 @app.post("/chat")
-def chat_endpoint(request: RequestState):
+def chat_endpoint(request: RequestState): 
     """
     API Endpoint to interact with the Chatbot using LangGraph and search tools.
-    It dynamically selects the model specified in the request.
+    It dynamically selects the model specified in the request
     """
     if request.model_name not in ALLOWED_MODEL_NAMES:
-        return {"error": "Model not allowed. Please use one of the allowed models."}
+        return {"error": "Invalid model name. Kindly select a valid AI model"}
     
-    response=get_response_from_ai_agent
+    llm_id = request.model_name
+    query = request.messages
+    allow_search = request.allow_search
+    system_prompt = request.system_prompt
+    provider = request.model_provider
+
+    # Create AI Agent and get response from it! 
+    response=get_response_from_ai_agent(llm_id, query, allow_search, system_prompt, provider)
+    return response
+
+#Step3: Run app & Explore Swagger UI Docs
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=9999)
